a80efa1dd0e1cba6fb44043cb0255f2f7c44194e
cianhiggins <cian.higgins10@gmail.com>
2020-12-15 20:58:54 +0000

    features: word_count, '?', '!'

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,28 @@ import pandas as pd
 import numpy as np
 
 csv_file = "data/small_set.csv"
-df = pd.read_csv(csv_file, comment='#', header=None, quotechar='"', skipinitialspace=True)
+df = pd.read_csv(csv_file, comment='#', quotechar='"', skipinitialspace=True, index_col=0)
 print(df.head())
-X = np.array(df.iloc[:, 0])
-X = X.reshape(-1, 1)
+headlines = np.array(df.iloc[:, 0])
+headlines = headlines.reshape(-1, 1)
 y = np.array(df.iloc[:, 2])
 y = y.reshape(-1, 1)
 
+
+def word_count(headline):
+    return len(headline.split())
+
+
+def question_mark(headline):
+    return int('?' in headline)
+
+
+def exclamation_mark(headline):
+    return int('!' in headline)
+
+
+df['word_count'] = df['headline'].apply(word_count)
+df['question'] = df['headline'].apply(question_mark)
+df['exclamtion'] = df['headline'].apply(exclamation_mark)
+
+print(df.head())

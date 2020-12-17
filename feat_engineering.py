157e509e7eb183a92da8fd53c7ee49b3c2bb796f
cianhiggins <cian.higgins10@gmail.com>
2020-12-17 18:04:16 +0000

    normalising data (minmaxscale)

--- feat_engineering.py
+++ feat_engineering.py
@@ -1,7 +1,20 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 raw_feats_csv = "data/features.csv"
 df = pd.read_csv(raw_feats_csv)
 print(df.head())
 
-# TODO normalisation of all feature columns - what type will we use?
+data = np.array(df.iloc[:, 1:])
+labels = np.array(df.iloc[:, 0])
+labels = labels.reshape(-1, 1)
+
+scaler = MinMaxScaler()
+scaler.fit(data)
+scaled_data = scaler.transform(data)
+
+df = pd.DataFrame(scaled_data)
+df.insert(0, None, labels)
+
+df.to_csv(path_or_buf="data/normalised.csv", index=False, header=False)
